7d59c7336d402b32efdef877f89cf7ec62fb8aa8
SelahG <71525491+SelahG@users.noreply.github.com>
2026-07-25 22:46:36 -0500

    Revert "Merge pull request #10 from SelahG/Selah_Wagon"
    This reverts commit 7f64280346cda3a22785a1be5d5555297b927b7b, reversing
    changes made to 974a933eba13c5ebca1818f8f9e520426a162ecb.

--- Assets/Scripts/Inventory/PlantCollectable.cs
+++ Assets/Scripts/Inventory/PlantCollectable.cs
@@ -6,6 +6,9 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 [RequireComponent(typeof(XRGrabInteractable))]
 public class PlantCollectable : MonoBehaviour
 {
+    [Header("Effects")]
+    public GameObject collectParticles;
+    public Transform particleSpawn;
 
     [Header("Input")]
     public InputActionReference depositButton;
@@ -65,13 +68,13 @@ public class PlantCollectable : MonoBehaviour
         if (!accepted)
             return;
 
-        // if (collectParticles != null)
-        // {
-        //     Instantiate(
-        //         collectParticles,
-        //         particleSpawn.position,
-        //         Quaternion.identity);
-        // }
+        if (collectParticles != null)
+        {
+            Instantiate(
+                collectParticles,
+                particleSpawn.position,
+                Quaternion.identity);
+        }
 
         Destroy(gameObject);
     }
